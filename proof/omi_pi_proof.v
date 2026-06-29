74d0699ffabcd8b787f95b33ebf76706cd7c6b37
Brian Thorne <bthornemail@gmail.com>
2026-06-29 15:42:03 -0700

    Refactor bqf definition into modular components and add new theorems for bqf bridge properties

--- proof/omi_pi_proof.v
+++ proof/omi_pi_proof.v
@@ -209,15 +209,30 @@ Proof.
   all: vm_compute; reflexivity.
 Qed.
 
-Definition bqf (x y : N) : N := 60 * x * x + 16 * x * y + 4 * y * y.
+Definition bqf_high_shell (x : N) : N := 60 * x * x.
+
+Definition bqf_chiral_bridge (x y : N) : N := 16 * x * y.
+
+Definition bqf_local_seed (y : N) : N := 4 * y * y.
+
+Definition bqf (x y : N) : N :=
+  bqf_high_shell x + bqf_chiral_bridge x y + bqf_local_seed y.
 
 Theorem bqf_decompose : forall x y : N,
   bqf x y = 4 * (15 * x * x + 4 * x * y + y * y).
 Proof.
   intros x y.
-  unfold bqf.
+  unfold bqf, bqf_high_shell, bqf_chiral_bridge, bqf_local_seed.
   nia.
 Qed.
+
+Theorem bqf_chiral_bridge_is_16xy : forall x y : N,
+  bqf_chiral_bridge x y = 16 * x * y.
+Proof. reflexivity. Qed.
+
+Theorem bqf_layer_sum : forall x y : N,
+  bqf x y = bqf_high_shell x + bqf_chiral_bridge x y + bqf_local_seed y.
+Proof. reflexivity. Qed.
 
 Record ProjectionBoundary : Type := mkProjectionBoundary {
   finite_incidence_exact : Prop;
@@ -386,6 +401,74 @@ Proof.
   reflexivity.
 Qed.
 
+Definition five_factorial_resolution : N := 120%N.
+
+Definition local240_resolution : N := (2 * five_factorial_resolution)%N.
+
+Theorem local240_is_two_5factorial :
+  local240_resolution = 240%N.
+Proof. vm_compute. reflexivity. Qed.
+
+Definition fano_selector (n : nat) : N := N.of_nat (n mod 7)%nat.
+
+Definition local240_selector (n : nat) : N := N.of_nat (n mod 240)%nat.
+
+Definition projection_denominator_index (n : nat) : N := N.of_nat (2 * n + 1).
+
+Record BQFBridge : Type := mkBQFBridge {
+  bridge_fano7 : N;
+  bridge_local240 : N;
+  bridge_phase : ChiralPhase;
+  bridge_denominator : N;
+  bridge_cross : N
+}.
+
+Definition bqf_bridge_at (n : nat) : BQFBridge :=
+  let x := (fano_selector n + 1)%N in
+  let y := (local240_selector n + 1)%N in
+  mkBQFBridge
+    (fano_selector n)
+    (local240_selector n)
+    (diagonal_accumulator_phase n)
+    (projection_denominator_index n)
+    (bqf_chiral_bridge x y).
+
+Theorem fano_selector_bound : forall n : nat,
+  (fano_selector n < 7)%N.
+Proof.
+  intro n.
+  unfold fano_selector.
+  apply N.compare_lt_iff.
+  change ((N.of_nat (n mod 7) ?= N.of_nat 7)%N = Lt).
+  rewrite <- Nat2N.inj_compare.
+  apply Nat.compare_lt_iff.
+  apply Nat.mod_upper_bound.
+  lia.
+Qed.
+
+Theorem local240_selector_bound : forall n : nat,
+  (local240_selector n < local240_resolution)%N.
+Proof.
+  intro n.
+  rewrite local240_is_two_5factorial.
+  unfold local240_selector.
+  apply N.compare_lt_iff.
+  change ((N.of_nat (n mod 240) ?= N.of_nat 240)%N = Lt).
+  rewrite <- Nat2N.inj_compare.
+  apply Nat.compare_lt_iff.
+  apply Nat.mod_upper_bound.
+  lia.
+Qed.
+
+Theorem bqf_bridge_phase_matches_accumulator : forall n : nat,
+  bridge_phase (bqf_bridge_at n) = diagonal_accumulator_phase n.
+Proof. reflexivity. Qed.
+
+Theorem bqf_bridge_cross_is_16xy : forall n : nat,
+  bridge_cross (bqf_bridge_at n) =
+    (16 * (fano_selector n + 1) * (local240_selector n + 1))%N.
+Proof. reflexivity. Qed.
+
 Theorem dplus_phase_sign : phase_to_sign DPlusPhase = 1.
 Proof. reflexivity. Qed.
 
@@ -400,6 +483,30 @@ Definition omi_pi_sign (n : nat) : R :=
   if omi_pi_phase_even n then 1 else -1.
 
 Definition omi_pi_den (n : nat) : R := INR (2 * n + 1).
+
+Theorem bqf_bridge_denominator_matches_projection : forall n : nat,
+  INR (N.to_nat (bridge_denominator (bqf_bridge_at n))) = omi_pi_den n.
+Proof.
+  intro n.
+  unfold bqf_bridge_at, projection_denominator_index, omi_pi_den.
+  simpl.
+  rewrite Nat2N.id.
+  reflexivity.
+Qed.
+
+Theorem bqf_bridge_carries_projection_schedule : forall n : nat,
+  bridge_phase (bqf_bridge_at n) = diagonal_accumulator_phase n /\
+  INR (N.to_nat (bridge_denominator (bqf_bridge_at n))) = omi_pi_den n /\
+  bridge_cross (bqf_bridge_at n) =
+    (16 * (fano_selector n + 1) * (local240_selector n + 1))%N.
+Proof.
+  intro n.
+  split.
+  - apply bqf_bridge_phase_matches_accumulator.
+  - split.
+    + apply bqf_bridge_denominator_matches_projection.
+    + apply bqf_bridge_cross_is_16xy.
+Qed.
 
 Definition omi_pi_term_from_incidence (n : nat) : R :=
   omi_pi_sign n / omi_pi_den n.
